05124d0d992054fae26dda7dbfbc794bb25dd516
Cheerpipe <cheerpipe@gmail.com>
2020-05-08 11:56:17 -0400

    Initial commit.

---
+++ MagicHomeAurora/MagicHomeAuroraDevice.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Drawing;
+
+
+namespace MagicHomeAurora
+{
+    public class MagicHomeAuroraDevice
+    {
+        private MagicHome.MagicHome _strips;
+        private string _ipAddress;
+        private Thread _newSetColorThread;
+        private Color _color;
+        private readonly ManualResetEvent _commandEvent = new ManualResetEvent(initialState: false);
+
+        public bool IsInitialized()
+        {
+            return _strips != null && _strips.IsConnected;
+        }
+
+        public void Init(string ipAddress)
+        {
+            timerDisconnect.AutoReset = false;
+            timerDisconnect.Enabled = false;
+            timerDisconnect.Elapsed += TimerDisconnect_Elapsed;
+            _ipAddress = ipAddress;
+            var initThread = new Thread(DoInit);
+            initThread.SetApartmentState(ApartmentState.STA);
+            initThread.Start();
+            initThread.Join();
+
+            _newSetColorThread = new Thread(SetColor);
+            _newSetColorThread.SetApartmentState(ApartmentState.STA);
+            _newSetColorThread.Start();
+        }
+
+        private void TimerDisconnect_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Disconnect();
+        }
+
+        public void Stop()
+        {
+            if (_strips.IsConnected)
+                _strips.Disconnect();
+
+            while (_strips.IsConnected)
+            {
+                Thread.Sleep(50);
+            }
+
+            if (_strips != null)
+                _strips.Dispose();
+            _newSetColorThread.Abort();
+        }
+
+        private void SetColor()
+        {
+            while (_newSetColorThread.IsAlive)
+            {
+                _commandEvent.WaitOne();
+                if (_strips.IsConnected)
+                    _strips.SetColor(_color);
+                _commandEvent.Reset();
+            }
+        }
+
+        public void SetColor(Color color)
+        {
+            if (!_strips.IsConnected)
+            {
+                Connect();
+            }
+            _color = color;
+            _commandEvent.Set();
+            timerDisconnect.Stop();
+            timerDisconnect.Start();
+        }
+
+        private void Disconnect()
+        {
+            if (_strips != null)
+                if (_strips.IsConnected)
+                    _strips.Disconnect();
+        }
+
+        System.Timers.Timer timerDisconnect = new System.Timers.Timer(5000);
+
+        private void Connect()
+        {
+            if (_strips != null)
+                if (!_strips.IsConnected)
+                    _strips.Connect();
+
+        }
+
+        private void DoInit()
+        {
+            _strips = new MagicHome.MagicHome();
+            if (!IPAddress.TryParse(_ipAddress, out _strips.Ip))
+            {
+                Console.WriteLine("Invalid IP Address");
+            }
+
+            Connect();
+            _strips.OnConnectFail += _strips_OnConnectFail;
+            _strips.OnConnectionLost += _strips_OnConnectionLost;
+            _strips.TurnOnWhenConnected = false;
+            System.Timers.Timer timeout = new System.Timers.Timer(5000);
+            timeout.Elapsed += Timeout_Elapsed;
+            timeout.Start();
+            while (!_strips.IsConnected && timeout.Enabled)
+            {
+                Thread.Sleep(50);
+            }
+            if (!_strips.IsConnected)
+            {
+                Console.WriteLine("Controller not found.");
+            }
+        }
+
+        private void _strips_OnConnectionLost(object sender, EventArgs e)
+        {
+            if (_strips != null)
+                if (!_strips.IsConnected)
+                    _strips.Connect();
+        }
+
+        private void _strips_OnConnectFail(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void Timeout_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            ((System.Timers.Timer)sender).Enabled = false;
+        }
+    }
+}
---
+++ MagicHomeAurora/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("MagicHomeAurora")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MagicHomeAurora")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("b4d045b0-f283-4bed-89b8-3da2b219792b")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
